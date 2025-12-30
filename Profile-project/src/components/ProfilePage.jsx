1ca2acc86296e3e9eb54567a002dfc9702c3d838
Farzana <akbarifarzana77@gmail.com>
2025-12-30 20:33:39 +0400

    Add StatCard components to ProfilePage
    Integrated StatCard components into the ProfilePage to display user streak information. Updated layout to include a stats grid and refactored TopHeader usage.

--- Profile-project/src/components/ProfilePage.jsx
+++ Profile-project/src/components/ProfilePage.jsx
@@ -1,11 +1,33 @@
 import TopHeader from "./TopHeader";
+import StatCard from "./StatCard";
 
 export default function ProfilePage({ user }) {
   return (
-    <TopHeader
+    <div className="layout">
+
+    
+    <TopHeader>
       name={user.name}
       username={user.username}
       avatarImage={user.avatarImage}
-    />
+    </TopHeader>
+    <div className="statsGrid">
+      <StatCard 
+      title="STREAK"
+      bigValue={`${user.streakDays} days`}
+      footerText={`Longest Streak: ${user.lastActive}`}>
+      icon="🔥"  
+      </StatCard>
+      
+      <StatCard 
+      title="STREAK"
+      bigValue={`${user.streakDays} days`}
+      footerText={`Longest Streak: ${user.lastActive}`}>
+      icon="🔥"  
+      </StatCard>
+
+    </div>
+
+    </div>
   )
 }
