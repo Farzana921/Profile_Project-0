2b065ff83db7e546132712a21d8375f2f5550d3f
Farzana <akbarifarzana77@gmail.com>
2025-12-30 20:16:21 +0400

    Fix typo in ProfilePage filename and update TopHeader props
    Renamed 'PofilePage.jsx' to 'ProfilePage.jsx' to correct a typo. Updated TopHeader to accept props as an object and adjusted its usage in ProfilePage accordingly.

--- Profile-project/src/components/TopHeader.jsx
+++ Profile-project/src/components/TopHeader.jsx
@@ -1,24 +1,21 @@
-export default function TopHeader(name, username, avatarImage){
-return (
+export default function TopHeader({ name, username, avatarImage }) {
+  return (
     <header className="topHeader">
-        <div className="topheaderInner">
-            <div className="avatarCircle avatarImageWrapper">
-                <img src={avatarImage} alt={name} className="avatarRing" />
-
-            </div>
-            <div className="topheadercont">
-                <h1 className="ProfileName">{name}</h1>
-                <p className="profileHandle">{username}</p>
-
-                <div className="topHeaderButtons">
-                    <button className="btnHeader primary">Edit profile</button>
-                    <button className="btnHeader ghost">Sign Out</button>
-                </div>
-
-            </div>
-
+      <div className="topheaderInner">
+        <div className="avatarCircle avatarImageWrapper">
+          <img src={avatarImage} alt={name} className="avatarRing" />
         </div>
 
+        <div className="topheadercont">
+          <h1 className="ProfileName">{name}</h1>
+          <p className="profileHandle">{username}</p>
+
+          <div className="topHeaderButtons">
+            <button className="btnHeader primary">Edit profile</button>
+            <button className="btnHeader ghost">Sign Out</button>
+          </div>
+        </div>
+      </div>
     </header>
-)
+  )
 }
