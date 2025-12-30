eebca312c6d5b13f756e40393ed9b32f2493396c
Farzana <akbarifarzana77@gmail.com>
2025-12-30 18:03:36 +0400

    Add Row component for displaying label and value
    Introduces a new Row component that renders a label and value pair within styled spans. This will help standardize row display in the UI.

---
+++ Profile-project/src/components/Row.jsx
@@ -0,0 +1,9 @@
+export default function Row(lable, value){
+    return (
+        <div className="row">
+            <span className="rowLable"> {lable}</span>
+            <span className="rowValue">{value}</span>
+        </div>
+    )
+
+}
